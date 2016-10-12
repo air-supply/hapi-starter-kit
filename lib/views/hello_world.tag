c61a1848674d8193a9957049db6c239bcf6efb4f
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-10-12 14:57:44 +0100

    adds joi validation example #7

--- lib/views/hello_world.tag
+++ lib/views/hello_world.tag
@@ -1,3 +1,8 @@
 <hello-world>
   <h1>HELLO YOU BEAUTIFUL WORLD</h1>
+  <form method="POST" url="/">
+    <input type="input" placeholder="Title" name="title" />
+    <input type="input" placeholder="Description" name="description" />
+    <input type="submit" value="Submit" />
+  </form>
 </hello-world>
--- test/routes.test.js
+++ test/routes.test.js
@@ -15,6 +15,38 @@ test('hello_world', function (t) {
   });
 });
 
+test('hello_world_post valid', function (t) {
+  var options = {
+    method: 'POST',
+    url: '/',
+    payload: {
+      title: 'testTitle',
+      description: 'testDescription'
+    }
+  };
+
+  server.inject(options, function (response) {
+    t.equal(response.statusCode, 200, '✅  200 response code returned');
+    t.end();
+  });
+});
+
+test('hello_world_post invalid', function (t) {
+  var options = {
+    method: 'POST',
+    url: '/',
+    payload: {
+      title: '',
+      description: ''
+    }
+  };
+
+  server.inject(options, function (response) {
+    t.equal(response.statusCode, 400, '✅  400 response code returned');
+    t.end();
+  });
+});
+
 test('stop server', function (t) {
   server.stop(t.end);
 });
